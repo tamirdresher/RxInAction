8aaf9645ee6ee1f87ab05a9873e7a70d5564a330
tamir.dresher@gmail.com <vhpupuyo>
2015-07-16 01:19:55 +0300

    more enumerable to observable examples

--- CH04/EnumerablesToObservables/Program.cs
+++ CH04/EnumerablesToObservables/Program.cs
@@ -5,6 +5,7 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using CreatingObservables.Chat;
 using Helpers;
@@ -20,7 +21,97 @@ namespace EnumerablesToObservables
             ThrowingEnumerable();
             MergingObservableConnectionWithLoadedMessages();
             ObservableToEnumerable();
+            ObservableToList();
+            ObservableToDictionary();
+            ObservableToLookup();
+            ObservableToEnumerableWithNext();
+
             Console.ReadLine();
+        }
+
+        private static void ObservableToEnumerableWithNext()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Observable To Enumerable (using Next) ----");
+
+            var observable =
+                 Observable.Interval(TimeSpan.FromSeconds(1));
+
+            var enumerable = observable.Next();
+
+            // enumerting on the enumerable - some values will be missed beacuse the thread will sleep 
+            // when they are pushed
+            // only 5 items are taken so we wont stay here forever
+            foreach (var item in enumerable.Take(5))
+            {
+                Console.WriteLine(item);
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+            }
+
+            Console.WriteLine("finished iterating");
+            Console.WriteLine("------------------");
+        }
+
+        private static void ObservableToLookup()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Observable To Lookup ----");
+            // Madrid and London has the same length
+            IEnumerable<string> cities = new[] { "London", "Tel-Aviv", "Tokyo", "Rome", "Madrid" };
+
+            var lookupObservable =
+                cities
+                .ToObservable()
+                .ToLookup(c => c.Length);
+
+            lookupObservable
+                .Select(lookup =>
+                {
+                    var groups = new StringBuilder();
+                    foreach (var grp in lookup)
+                        groups.AppendFormat("[Key:{0} => {1}]", grp.Key, grp.Count());
+                    return groups.ToString();
+                })
+                .SubscribeConsole("lookup");
+        }
+
+        private static void ObservableToDictionary()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Observable To Dictionary ----");
+            IEnumerable<string> cities = new[] { "London", "Tel-Aviv", "Tokyo", "Rome" };
+
+            var dictionaryObservable =
+                cities
+                .ToObservable()
+                .ToDictionary(c => c.Length);//change the value to some const to see and error
+
+            dictionaryObservable
+                .Select(d => string.Join(",", d))
+                .SubscribeConsole("dictionary");
+        }
+
+        private static void ObservableToList()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Observable To List ----");
+            var observable =
+                Observable.Create<string>(o =>
+                {
+                    o.OnNext("Observable");
+                    o.OnNext("To");
+                    o.OnNext("List");
+                    //  comment the call to OnCompleted() to see how the list is never built 
+                    o.OnCompleted();
+                    return Disposable.Empty;
+                });
+
+            IObservable<IList<string>> listObservable =
+                observable.ToList();
+
+            listObservable
+                .Select(lst => string.Join(",", lst))
+                .SubscribeConsole("list ready");
         }
 
         private static void ObservableToEnumerable()
@@ -28,22 +119,28 @@ namespace EnumerablesToObservables
             Console.WriteLine();
             Console.WriteLine("----- Observable To Enumerable ----");
 
-var observable =
-    Observable.Create<string>(o =>
-    {
-        o.OnNext("Observable");
-        o.OnNext("To");
-        o.OnNext("Enumerable");
-        //  comment the call to OnCompleted() to see the thread wait
-        o.OnCompleted();
-        return Disposable.Empty;
-    });
+            var observable =
+                Observable.Create<string>(o =>
+                {
+                    o.OnNext("Observable");
+                    o.OnNext("To");
+                    o.OnNext("Enumerable");
+                    //  comment the call to OnCompleted() to see the thread wait
+                    o.OnCompleted();
+                    return Disposable.Empty;
+                });
 
-var enumerable = observable.ToEnumerable();
-foreach (var item in enumerable)
-{
-    Console.WriteLine(item);
-}
+            var enumerable = observable.ToEnumerable();
+            Console.WriteLine("got the enumerable");
+            Console.WriteLine("------------------");
+
+            foreach (var item in enumerable)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("finished iterating");
+            Console.WriteLine("------------------");
         }
 
         private static void EnumerableToObservable()
