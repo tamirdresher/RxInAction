46edad975285b7e45b3738e50717002b9fc353f2
tamir.dresher@gmail.com <vhpupuyo>
2015-07-15 22:33:30 +0300

    missing files

---
+++ CH04/EnumerablesToObservables/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CreatingObservables.Chat;
+using Helpers;
+
+namespace EnumerablesToObservables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnumerableToObservable();
+            SubscribingToEnumerable();
+            ThrowingEnumerable();
+            MergingObservableConnectionWithLoadedMessages();
+            ObservableToEnumerable();
+            Console.ReadLine();
+        }
+
+        private static void ObservableToEnumerable()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Observable To Enumerable ----");
+
+var observable =
+    Observable.Create<string>(o =>
+    {
+        o.OnNext("Observable");
+        o.OnNext("To");
+        o.OnNext("Enumerable");
+        //  comment the call to OnCompleted() to see the thread wait
+        o.OnCompleted();
+        return Disposable.Empty;
+    });
+
+var enumerable = observable.ToEnumerable();
+foreach (var item in enumerable)
+{
+    Console.WriteLine(item);
+}
+        }
+
+        private static void EnumerableToObservable()
+        {
+            Console.WriteLine("----- Enumerable to Observable ----");
+
+            IEnumerable<string> names = new[] { "Shira", "Yonatan", "Gabi", "Tamir" };
+            IObservable<string> observable = names.ToObservable();
+
+            observable.SubscribeConsole("names");
+        }
+
+        private static void SubscribingToEnumerable()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Subscribing to enumerable ----");
+
+            IEnumerable<string> names = new[] { "Shira", "Yonatan", "Gabi", "Tamir" };
+            names.Subscribe(new ConsoleObserver<string>("subscribe"));
+        }
+
+        private static void ThrowingEnumerable()
+        {
+            //this shows that exception that happen while iterating are sent to the OnError
+            NumbersAndThrow()
+                .ToObservable()
+                .SubscribeConsole("enumerable with exception");
+        }
+
+        static IEnumerable<int> NumbersAndThrow()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Numbers and Throw ----");
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            throw new ApplicationException("Something Bad Happened");
+            yield return 4;
+        }
+
+        static void MergingObservableConnectionWithLoadedMessages()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Merging ObservableConnection with loaded messages ----");
+
+            ChatClient client = new ChatClient();
+            IObservable<string> liveMessages = client.ObserveMessages("user", "pass");
+            IEnumerable<string> loadedMessages = LoadMessagesFromDB();
+
+            loadedMessages.ToObservable()
+                .Concat(liveMessages)
+                .SubscribeConsole("merged");
+
+            //this another way to do the same
+            liveMessages.
+                StartWith(loadedMessages)
+                .SubscribeConsole("loaded first");
+
+            client.NotifyRecieved("live message1");
+            client.NotifyRecieved("live message2");
+        }
+
+        private static IEnumerable<string> LoadMessagesFromDB()
+        {
+            yield return "loaded1";
+            yield return "loaded2";
+        }
+    }
+}
---
+++ CH04/NonStandardEvents/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace NonStandardEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new WifiScanner();
+
+            //
+            // this code snippet will result in an ArgumentException, since the NetworkFoundEventHandler delegate is not convertible to the standard EventHandler<TEventArgs> type
+            //
+            //var networks = Observable.FromEventPattern<NetworkFoundEventHandler, string>(
+            //      (h) => { listener.NetworkFound += h; },
+            //      (h) => { listener.NetworkFound -= h; });
+            //networks.SubscribeConsole();
+
+            //
+            // when the target event has only one parameter, its easy to make it into observable
+            //
+            //IObservable<string> networks = Observable.FromEvent<NetworkFoundEventHandler, string>(
+            //      (h) => { listener.NetworkFound += h; },
+            //      (h) => { listener.NetworkFound -= h; });
+
+            //
+            // When the target event has more than one parameter, a conversion method is needed to turn them into a single object
+            //
+            IObservable<Tuple<string, int>> networks = Observable.FromEvent<ExtendedNetworkFoundEventHandler, Tuple<String, int>>(
+                rxHandler =>
+                    (ssid, strength) => rxHandler(Tuple.Create(ssid, strength)),
+                h => listener.ExtendedNetworkFound += h,
+                h => listener.ExtendedNetworkFound -= h);
+
+
+            networks.SubscribeConsole();
+            networks.SubscribeConsole();
+
+            while (true)
+            {
+                Console.WriteLine("Enter the network ssid or X to exit");
+                var ssid = Console.ReadLine();
+                if (ssid == "X")
+                {
+                    break;
+                }
+                Console.WriteLine("Enter the network strength - 1 to 10");
+                var strength = int.Parse(Console.ReadLine());
+
+                listener.RaiseFound(ssid, strength);
+            }
+        }
+
+
+    }
+
+    public delegate void NetworkFoundEventHandler(string ssid);
+    public delegate void ExtendedNetworkFoundEventHandler(string ssid, int strength);
+    class WifiScanner
+    {
+        public event NetworkFoundEventHandler NetworkFound = delegate { };
+        public event ExtendedNetworkFoundEventHandler ExtendedNetworkFound = delegate { };
+
+        // rest of the code
+        public void RaiseFound(string ssid, int strength = 0)
+        {
+            NetworkFound(ssid);
+            ExtendedNetworkFound(ssid, strength);
+
+
+        }
+    }
+}
