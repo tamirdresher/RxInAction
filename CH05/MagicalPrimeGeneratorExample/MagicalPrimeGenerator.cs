b093f8f7602681d13f4aa9697f0ebafc92132ebb
tamir.dresher@gmail.com <vhpupuyo>
2015-08-01 09:46:18 +0300

    primes generator example

---
+++ CH05/MagicalPrimeGeneratorExample/MagicalPrimeGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MagicalPrimeGeneratorExample
+{
+    class MagicalPrimeGenerator
+    {
+        public IEnumerable<int> Generate(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                yield return GeneratePrime(i);
+            }
+        }
+
+        public IObservable<int> ObservePrimes(int amount)
+        {
+            CancellationTokenSource cts=new CancellationTokenSource(); 
+            return Observable.Create<int>(o =>
+            {
+                Task.Run(() =>
+                {
+                    foreach (var prime in Generate(amount))
+                    {
+                        cts.Token.ThrowIfCancellationRequested();
+                        o.OnNext(prime);
+                    }
+                    o.OnCompleted();
+                });
+
+                return new CancellationDisposable(cts);
+            });
+        } 
+
+        int GeneratePrime(int index)
+        {
+            // Simulate the hard work 
+            Thread.Sleep(2000);
+
+            var firstNumbers = new[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, };
+            if (index<firstNumbers.Length)
+            {
+                return firstNumbers[index];
+            }
+            
+            // THIS IS JUST FOR DEMONSTRATION, I DONT REALLY HAVE MAGICAL GENERATOR
+            return firstNumbers.Last();
+        }
+       
+        
+    }
+}
---
+++ CH05/MagicalPrimeGeneratorExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace MagicalPrimeGeneratorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GeneratingSynchronously();
+            GeneratingAsynchronously();
+            Console.ReadLine();
+        }
+
+        private static void GeneratingSynchronously()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("----- Using synchronous enumerable ----");
+
+            var generator = new MagicalPrimeGenerator();
+            // this will block the main thread for a few seconds
+            foreach (var prime in generator.Generate(5))
+            {
+                Console.Write("{0}, ", prime);
+            }
+        }
+
+        private static void GeneratingAsynchronously()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Using observable ----");
+
+            var generator = new MagicalPrimeGenerator();
+
+            var subscription=generator.ObservePrimes(5)
+                .SubscribeConsole("async observable");
+
+            Console.WriteLine("Proving we're not blocked. you can continue typing. type X to dispose and exit");
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "X")
+                {
+                    subscription.Dispose();
+                    return;
+                }
+
+                Console.WriteLine("\t {0}", input);
+            }
+        }
+        
+    }
+}
