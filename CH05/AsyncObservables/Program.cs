e4547122e3cdf2b5b249ec805660c60525594abc
tamir.dresher@gmail.com <vhpupuyo>
2015-08-06 00:43:24 +0300

    async examples - Concat and SelectMany

---
+++ CH05/AsyncObservables/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace AsyncObservables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uncomment the example you wish to run
+
+            //SearchingWithAsyncAwait();
+            //SearchingWithCancellation();
+            //SearchingWithConcatingTasks();
+            //SearchingWithDefferedAsync();
+
+            RunningAsyncCodeInWhere();
+            Console.ReadLine();
+        }
+
+        private static void RunningAsyncCodeInWhere()
+        {
+            var svc = new PrimeCheckService();
+            //
+            // this wont Compile
+            //
+            //var subscription = Observable.Range(1, 10)
+            //    .Where(async x => await svc.IsPrimeAsync(x))
+            //    .SubscribeConsole("AsyncWhere");
+            IObservable<int> primes;
+            primes = from number in Observable.Range(1, 10)
+                     from isPrime in svc.IsPrimeAsync(number)
+                     where isPrime
+                     select number;
+
+            primes =
+                Observable.Range(1, 10)
+                    .SelectMany((number) => svc.IsPrimeAsync(number),
+                                 (number, isPrime) => new { number, isPrime })
+                    .Where(x => x.isPrime)
+                    .Select(x => x.number);
+
+
+            primes =
+                Observable.Range(1, 10)
+                    .Select(async (number) => new {number, IsPrime = await svc.IsPrimeAsync(number)})
+                    .Concat()
+                    .Where(x => x.IsPrime)
+                    .Select(x => x.number);
+
+            primes.SubscribeConsole("primes");
+        }
+
+        private class PrimeCheckService
+        {
+            public async Task<bool> IsPrimeAsync(int number)
+            {
+                return await Task.Run(async () =>
+                 {
+                     if (number == 3)
+                     {
+                         await Task.Delay(2000);
+                     }
+                     for (int j = 2; j <= Math.Sqrt(number); j++)
+                     {
+                         if (number % j == 0)
+                         {
+                             return false;
+                         }
+                     }
+                     return true;
+                 });
+            }
+        }
+
+        public static void SearchingWithAsyncAwait()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Creating async observable with async-await ----");
+
+            var results = SearchEngineExample.Search_WithAsyncAwait("Rx");
+            int i = 0;
+            var subscription = Disposable.Empty;
+            subscription =
+                results.SubscribeConsole("results");
+        }
+
+        public static void SearchingWithCancellation()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Creating async observable with async-await and cancellation----");
+
+
+            // Change the index to when you want the subscription disposed
+            int cancelIndex = 1;
+
+            var results = SearchEngineExample.Search_WithCancellation("Rx");
+
+            IDisposable subscription = Disposable.Empty;
+            subscription = results
+                .Select((result, index) => new { result, index }) //adding the item index to the notification
+                .Do(x =>
+                {
+                    if (x.index == cancelIndex)
+                    {
+                        Console.WriteLine("Cancelling on index {0}", cancelIndex);
+                        subscription.Dispose();
+                    }
+                })
+                .Select(x => x.result) //rollback the observable to be IObservable<string> 
+                .SubscribeConsole("results");
+
+        }
+
+        public static void SearchingWithConcatingTasks()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Converting Tasks to observables ----");
+
+            var results = SearchEngineExample.Search_ConcatingTasks("Rx");
+            int i = 0;
+            var subscription = Disposable.Empty;
+            subscription =
+                results.SubscribeConsole("results");
+        }
+
+        public static void SearchingWithDefferedAsync()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Defferd async ----");
+
+            var results = SearchEngineExample.Search_DefferedConcatingTasks("Rx");
+            int i = 0;
+            var subscription = Disposable.Empty;
+            subscription =
+                results.SubscribeConsole("results");
+        }
+    }
+}
---
+++ CH05/AsyncObservables/SearchEngineExample.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace AsyncObservables
+{
+    class SearchEngineExample
+    {
+      
+
+        public static IObservable<string> Search_WithAsyncAwait(string term)
+        {
+            return Observable.Create<string>(async o =>
+            {
+                var searchEngineA = new SearchEngineA();
+                var searchEngineB = new SearchEngineB();
+                var resultsA = await searchEngineA.SearchAsync(term);
+                foreach (var result in resultsA)
+                {
+                    o.OnNext(result);
+                }
+                var resultsB = await searchEngineB.SearchAsync(term);
+                foreach (var result in resultsB)
+                {
+                    o.OnNext(result);
+                }
+                o.OnCompleted();
+            });
+        }
+
+        public static IObservable<string> Search_WithCancellation(string term)
+        {
+            return Observable.Create<string>(async (o, cancellationToken) =>
+            {
+                var searchEngineA = new SearchEngineA();
+                var searchEngineB = new SearchEngineB();
+                var resultsA = await searchEngineA.SearchAsync(term);
+                foreach (var result in resultsA)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    o.OnNext(result);
+                }
+                cancellationToken.ThrowIfCancellationRequested();
+                var resultsB = await searchEngineB.SearchAsync(term);
+                foreach (var result in resultsB)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    o.OnNext(result);
+                }
+                o.OnCompleted();
+            });
+        }
+
+
+public static IObservable<string> Search_ConcatingTasks(string term)
+{
+    var searchEngineA = new SearchEngineA();
+    var searchEngineB = new SearchEngineB();
+    IObservable<IEnumerable<string>> resultsA = searchEngineA.SearchAsync(term).ToObservable();
+    IObservable<IEnumerable<string>> resultsB = searchEngineB.SearchAsync(term).ToObservable();
+    return resultsA
+        .Concat(resultsB)
+        .SelectMany(x => x);
+}
+
+        public static IObservable<string> Search_DefferedConcatingTasks(string term)
+        {
+            var searchEngineA = new SearchEngineA();
+            var searchEngineB = new SearchEngineB();
+            var resultsA = Observable.Defer(() => searchEngineA.SearchAsync(term).ToObservable());
+            var resultsB = Observable.Defer(() => searchEngineB.SearchAsync(term).ToObservable());
+            return resultsA
+                .Concat(resultsB)
+                .SelectMany(x => x);
+        }
+
+
+        class SearchEngineA : ISearchEngine
+        {
+            public async Task<IEnumerable<string>> SearchAsync(string term)
+            {
+                Console.WriteLine("SearchEngine A - SearchAsync()");
+
+                await Task.Delay(1500);//simulate latency
+                return new[] { "resultA", "resultB" };
+            }
+        }
+
+        class SearchEngineB : ISearchEngine
+        {
+            public async Task<IEnumerable<string>> SearchAsync(string term)
+            {
+                Console.WriteLine("SearchEngine B - SearchAsync()");
+                await Task.Delay(1500);//simulate latency
+                return new[] { "resultC", "resultD" }.AsEnumerable();
+            }
+        }
+    }
+
+    internal interface ISearchEngine
+    {
+        Task<IEnumerable<string>> SearchAsync(string term);
+    }
+}
