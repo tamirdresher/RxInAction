cec77e9b1cbd7cdaa50a52aa1e66136231cb50bf
tamir.dresher@gmail.com <vhpupuyo>
2015-09-23 12:07:50 +0300

    missing files

--- CH02/StockTrade/YourFirstRxExample/RxStockMonitor.cs
+++ CH02/StockTrade/YourFirstRxExample/RxStockMonitor.cs
@@ -7,55 +7,54 @@ using System.Threading.Tasks;
 
 namespace FirstRxExample
 {
-class RxStockMonitor : IDisposable
-{
-    private IDisposable _subscription;
-
-    public RxStockMonitor(StockTicker ticker)
+    class RxStockMonitor : IDisposable
     {
-        const decimal maxChangeRatio = 0.1m;
+        private IDisposable _subscription;
 
-IObservable<StockTick> ticks = 
-        Observable.FromEventPattern<EventHandler<StockTick>, StockTick>(
-            h => ticker.StockTick += h, //#A
-            h => ticker.StockTick -= h) //#B
-            .Select(tickEvent => tickEvent.EventArgs)
-            .Synchronize();
+        public RxStockMonitor(StockTicker ticker)
+        {
+            const decimal maxChangeRatio = 0.1m;
 
-        IObservable<IGroupedObservable<string, StockTick>> groupedTicks =
-            ticks.GroupBy(tick => tick.QuoteSymbol); 
-        var drasticChanges =
-            from tick in ticks
-            group tick by tick.QuoteSymbol
-            into company
-            from tickPair in company.Buffer(2, 1)
-            let changeRatio = Math.Abs((tickPair[1].Price - tickPair[0].Price)/tickPair[0].Price)
-            where changeRatio > maxChangeRatio
-            select new DrasticChange()
-            {
-                Symbol = company.Key,
-                ChangeRatio = changeRatio,
-                OldPrice = tickPair[0].Price,
-                NewPrice = tickPair[1].Price
-            };
-
-        _subscription =
-            drasticChanges.Subscribe(change =>
+            //creating an observable from the StockTick event, each notification will carry only the eventargs and will be synchronized
+            IObservable<StockTick> ticks =
+                    Observable.FromEventPattern<EventHandler<StockTick>, StockTick>(
+                        h => ticker.StockTick += h, 
+                        h => ticker.StockTick -= h) 
+                        .Select(tickEvent => tickEvent.EventArgs)
+                        .Synchronize();
+            
+            var drasticChanges =
+                from tick in ticks
+                group tick by tick.QuoteSymbol
+                into company
+                from tickPair in company.Buffer(2, 1)
+                let changeRatio = Math.Abs((tickPair[1].Price - tickPair[0].Price) / tickPair[0].Price)
+                where changeRatio > maxChangeRatio
+                select new DrasticChange()
                 {
-                    Console.WriteLine("Stock:{0} has changed with {1} ratio, Old Price:{2} New Price:{3}", change.Symbol,
-                        change.ChangeRatio,
-                        change.OldPrice,
-                        change.NewPrice);
-                },
-                ex => { /* code that handles erros */}, //#C
-                () =>{/* code that handles the observable completenss */}); //#C
+                    Symbol = company.Key,
+                    ChangeRatio = changeRatio,
+                    OldPrice = tickPair[0].Price,
+                    NewPrice = tickPair[1].Price
+                };
+
+            _subscription =
+                drasticChanges.Subscribe(change =>
+                    {
+                        Console.WriteLine("Stock:{0} has changed with {1} ratio, Old Price:{2} New Price:{3}", change.Symbol,
+                            change.ChangeRatio,
+                            change.OldPrice,
+                            change.NewPrice);
+                    },
+                    ex => { /* code that handles erros */}, //#C
+                    () => {/* code that handles the observable completenss */}); //#C
+        }
+
+        public void Dispose()
+        {
+            _subscription.Dispose();
+        }
     }
-    
-    public void Dispose()
-    {
-        _subscription.Dispose();
-    }
-}
 
     internal class DrasticChange
     {
---
+++ CH06/HotAndColdObservables/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace HotAndColdObservables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColdObservableMultipleSubscriptionsExample().Wait();
+            HeatingAnObservable();
+            PublishWithInitialValue();
+            PublishWithSelector();
+            PublishLast();
+            DisposingTheConnectableSubscription();
+            RefCount();
+            ReplayFive();
+
+            Console.ReadLine();
+        }
+
+        private static void ReplayFive()
+        {
+            Demo.DisplayHeader("Replay(2) - will replay the last two items for the subscribed observer");
+
+            var publishedObservable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(7)
+                .Replay(2);
+            publishedObservable.Connect();
+            var subscription1 = publishedObservable.SubscribeConsole("First");
+            Thread.Sleep(3000);
+            Console.WriteLine("subscribing the second observable");
+            var subscription2 = publishedObservable.SubscribeConsole("Second");
+
+            //waiting for the observable to complete
+            Thread.Sleep(4000);
+        }
+
+        private static void RefCount()
+        {
+            Demo.DisplayHeader("Automatic disconnection with RefCount");
+
+            //unbounded observable
+            var publishedObservable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Do(x => Console.WriteLine("Generating {0}", x))
+                .Publish()
+                .RefCount();
+
+            var subscription1 = publishedObservable.SubscribeConsole("First");
+            var subscription2 = publishedObservable.SubscribeConsole("Second");
+
+            //waiting 3 seconds before disposing one subscription
+            Thread.Sleep(3000);
+            Console.WriteLine("Disposing one subscription");
+            subscription1.Dispose();
+
+            //waiting 3 seconds before disposing the second subscription
+            Thread.Sleep(3000);
+            Console.WriteLine("Disposing the second subscription");
+            subscription2.Dispose();
+
+
+
+        }
+
+        private static void DisposingTheConnectableSubscription()
+        {
+            Demo.DisplayHeader("Reconnecting a connectable observable");
+
+            var connectableObservable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Take(5)
+                .Publish();
+
+            connectableObservable.SubscribeConsole("First");
+
+            var subscription = connectableObservable.Connect();
+            Thread.Sleep(3000);
+            Console.WriteLine("Disposing the connectable observable subscription");
+            subscription.Dispose();
+
+            Console.WriteLine("Waiting two seconds before reconnecting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Reconnecting");
+            subscription = connectableObservable.Connect();
+
+            //waiting for the observable to complete
+            Thread.Sleep(5000);
+        }
+
+        private static void Multicast()
+        {
+            var coldObservable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+
+            var observable =
+                coldObservable.Multicast(
+                    subjectSelector: () => new Subject<long>(),
+                    selector: src => src.Zip(src, (a, b) => a + b));
+
+            observable.SubscribeConsole();
+
+        }
+
+        private static void PublishLast()
+        {
+            Demo.DisplayHeader("PublishLast - will emit the last value, even for observers that subscribe after completed");
+
+            //an observable that simulate an asynchronous operation that take a long time to complete
+            var coldObservable = Observable.Timer(TimeSpan.FromSeconds(5)).Select(_ => "Rx");
+
+            Console.WriteLine("Creating hot disconncted observable");
+            var connectableObservable = coldObservable.PublishLast();
+
+
+            Console.WriteLine("Subscribing two observers now, and the third after the source observable completed");
+            connectableObservable.SubscribeConsole("First");
+            connectableObservable.SubscribeConsole("Second");
+
+            Console.WriteLine("Connecting the observable");
+            connectableObservable.Connect();
+
+            //waiting 6 seconds before subscribing a third observer
+            Thread.Sleep(6000);
+            Console.WriteLine("Subscribing the third observer - it will receive the last value");
+            connectableObservable.SubscribeConsole("Third");
+
+
+        }
+
+        private static void PublishWithInitialValue()
+        {
+            Demo.DisplayHeader("Publish(initial-value) - published the observable but using BehaviorSubject");
+
+            var coldObservable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+
+            Console.WriteLine("Creating hot disconncted observable");
+            var connectableObservable = coldObservable.Publish(-10);
+
+
+            Console.WriteLine("Subscribing two observers now, and the third in two more seconds");
+            connectableObservable.SubscribeConsole("First");
+            connectableObservable.SubscribeConsole("Second");
+
+            Console.WriteLine("Connecting the observable");
+            connectableObservable.Connect();
+
+            //waiting two seconds before subscribing a third observer
+            Thread.Sleep(2000);
+            Console.WriteLine("Subscribing the third observer - it will receive the current value");
+            connectableObservable.SubscribeConsole("Third");
+
+            //waiting for the observable sequence to complete
+            Thread.Sleep(3000);
+        }
+
+        private static void PublishWithSelector()
+        {
+            Demo.DisplayHeader("Publish(selector) - is good for reusing a published observable to create a new observable");
+
+            int i = 0;
+            var numbers = Observable.Range(1, 5).Select(_ => i++);//this observable causes a side effect
+
+
+            //since the 'numbers' observable is cold, this will result in the sequence of values in the form i+(i+1) and not i+i
+            var zipped = numbers.Zip(numbers, (a, b) => a + b);
+            zipped.SubscribeConsole("zipped");
+
+
+
+            Console.WriteLine("Zipping an observable to itself after publihsing it");
+            i = 0;
+            var publishedZip =
+                numbers.Publish(ys => ys.Zip(ys, (a, b) => a + b));
+            publishedZip.SubscribeConsole("publishedZipped");
+
+        }
+
+
+
+
+        private static void HeatingAnObservable()
+        {
+            Demo.DisplayHeader("Heating an observable using Publish and Connect");
+
+            var coldObservable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+
+            Console.WriteLine("Creating hot disconncted observable");
+            var connectableObservable = coldObservable.Publish();
+
+            Console.WriteLine("Subscribing two observers now, and the third in two more seconds");
+            connectableObservable.SubscribeConsole("First");
+            connectableObservable.SubscribeConsole("Second");
+
+            Console.WriteLine("Connecting the observable");
+            connectableObservable.Connect();
+
+            //waiting two seconds before subscribing a third observer
+            Thread.Sleep(2000);
+            Console.WriteLine("Subscribing the third observer - it will receive the next notification");
+            connectableObservable.SubscribeConsole("Third");
+
+            //waiting for the observable sequence to complete
+            Thread.Sleep(3000);
+        }
+
+        public static async Task ColdObservableMultipleSubscriptionsExample()
+        {
+            Demo.DisplayHeader("Cold observable will regenerate the entire seqeunce of notfications for each observer");
+
+            var coldObservable =
+                Observable.Create<string>(async o =>
+                {
+                    o.OnNext("Hello");
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    o.OnNext("Rx");
+                });
+
+            coldObservable.SubscribeConsole("o1");
+            await Task.Delay(TimeSpan.FromSeconds(0.5));
+            coldObservable.SubscribeConsole("o2");
+
+            //waiting for the observable sequence to complete
+            Thread.Sleep(3000);
+
+        }
+    }
+}
