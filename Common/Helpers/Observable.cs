1c667d1fd4bd5f3a7a56c77408ee9de9df456a2a
tamir.dresher@gmail.com <vhpupuyo>
2015-11-28 00:04:16 +0200

    Combining observables

---
+++ Common/Helpers/Observable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableEx
+    {
+        public static IObservable<T> FromValues<T>(params T[] values)
+        {
+            return values.ToObservable();
+        } 
+    }
+}
