6f13f949db0a73429086bc51da7a199b6ca9c560
tamir.dresher@gmail.com <vhpupuyo>
2015-12-22 23:56:57 +0200

    Examples: 1. What is a Scheduler 2. Scheduler Types - NewThread

---
+++ CH10/Schedulers/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace Schedulers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    BasicScheduling();
+            //    BasicSchedulingEveryTwoSeconds();
+            //    ObservableIntervalOnCurrentThread();
+            ParametrizedConcurrency();
+
+            TypesOfSchedulersExamples.Run();
+            Console.WriteLine("Press <Enter> to continue...");
+            Console.ReadLine();
+        }
+
+        private static void BasicScheduling()
+        {
+            Demo.DisplayHeader("Basic Scheduling - Scheduling an action to run after two seconds");
+
+            IScheduler scheduler = NewThreadScheduler.Default;
+
+            IDisposable scheduling =
+                scheduler.Schedule(
+                    Unit.Default,
+                    TimeSpan.FromSeconds(2),
+                    (scdlr, _) =>
+                    {
+                        Console.WriteLine("Hello World, Now: {0}", scdlr.Now);
+                        return Disposable.Empty;
+                    });
+
+            Console.WriteLine("sleeping for 3 seconds so the scheduling will take place");
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+        }
+
+        private static void BasicSchedulingEveryTwoSeconds()
+        {
+            Demo.DisplayHeader("Basic Scheduling - Scheduling an action to run recursively every two seconds");
+
+            IScheduler scheduler = NewThreadScheduler.Default;
+            Func<IScheduler, int, IDisposable> action = null;
+            action = (scdlr, callNumber) =>
+              {
+                  Console.WriteLine("Hello {0}, Now: {1}, Thread: {2}", callNumber, scdlr.Now, Thread.CurrentThread.ManagedThreadId);
+                  return scdlr.Schedule(callNumber + 1, TimeSpan.FromSeconds(2), action);
+              };
+            var scheduling =
+                scheduler.Schedule(
+                    0,
+                    TimeSpan.FromSeconds(2),
+                    action);
+
+            Console.WriteLine("sleeping for 5 seconds and then disposing");
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            scheduling.Dispose();
+
+            Console.WriteLine("scheduling disposed, Now: {0}", scheduler.Now);
+        }
+
+        private static void ObservableIntervalOnCurrentThread()
+        {
+            Demo.DisplayHeader("Parametrizing concurrency - passing the CurrentThreadScheduler to the Interval operator so the emissions will be on the calling thread");
+
+            Console.WriteLine("Before - Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Observable.Interval(TimeSpan.FromSeconds(1), CurrentThreadScheduler.Instance)
+                .Timestamp()
+                .Take(3)
+                .Do(x => Console.WriteLine("Inside - {0} - Thread: {1}", x, Thread.CurrentThread.ManagedThreadId))
+                .RunExample();
+        }
+
+        public static void ParametrizedConcurrency()
+        {
+            Demo.DisplayHeader("Parametrizing concurrency - passing the NewThreadScheduler to the Range operator so the emissions will be on another thread");
+
+var subscription =
+    Observable.Range(1, 5,NewThreadScheduler.Default)//without passing the scheduler, this will run infinitely
+        .Repeat()
+        .SubscribeConsole("Range on another thread");
+Thread.Sleep(TimeSpan.FromMilliseconds(50));
+subscription.Dispose();
+        }
+
+
+    }
+}
---
+++ CH10/Schedulers/TypesOfSchedulersExamples.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace Schedulers
+{
+    static class TypesOfSchedulersExamples
+    {
+        public static void Run()
+        {
+            NewThreadSchedulerExample();
+        }
+
+        private static void NewThreadSchedulerExample()
+        {
+            Demo.DisplayHeader("NewThreadScheduler - Creates a new thread for each scheduling");
+
+var newThreadScheduler = NewThreadScheduler.Default;
+
+newThreadScheduler.Schedule(Unit.Default,
+    (s, _) =>
+    {
+        Console.WriteLine("Action1 - Thread:{0}", Thread.CurrentThread.ManagedThreadId);
+    });
+newThreadScheduler.Schedule(Unit.Default,
+    (s, _) =>
+    {
+        Console.WriteLine("Action2 - Thread:{0}", Thread.CurrentThread.ManagedThreadId);
+    });
+        }
+    }
+}
