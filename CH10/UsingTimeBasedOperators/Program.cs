15af9154088f4789deed1469ef74dfb971de933a
tamir.dresher@gmail.com <vhpupuyo>
2015-12-27 08:38:05 +0200

    Delay and Throttling examples were improved

--- CH10/UsingTimeBasedOperators/Program.cs
+++ CH10/UsingTimeBasedOperators/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,23 +15,27 @@ namespace UsingTimeBasedOperators
     {
         static void Main(string[] args)
         {
-            AddingATimestampToNotifications();
-            AddingTimeIntervalBetweenNotifications();
+            //AddingATimestampToNotifications();
+            //AddingTimeIntervalBetweenNotifications();
             //UsingTimeInterval();
-            UsingTimeout();
-            //UsingDelay();
+            //UsingTimeout();
+            //DelayingNotifications();
+            //DelayingEachNotificationIndependently();
             //Throttling();
+            //VariableThrottling();
             SamplingTheObservable();
         }
+
+
 
         private static void SamplingTheObservable()
         {
             Demo.DisplayHeader("The Sample operator - samples the observable sequence every time-interval, emitting the last item in that interval");
 
-            Observable.Interval(TimeSpan.FromSeconds(1))
-                .Sample(TimeSpan.FromSeconds(3.5))
-                .Take(3)
-                .RunExample("Sample");
+Observable.Interval(TimeSpan.FromSeconds(1))
+    .Sample(TimeSpan.FromSeconds(3.5))
+    .Take(3)
+    .RunExample("Sample");
 
             //Same as
             Observable.Interval(TimeSpan.FromSeconds(1))
@@ -44,18 +49,40 @@ namespace UsingTimeBasedOperators
             Demo.DisplayHeader("Throttle operator - only emit an item from an Observable if a particular timespan has passed without it emitting another item");
 
             var observable = Observable
-              .Return(1)
-              .Concat(Observable.Timer(TimeSpan.FromSeconds(2)).Select(_ => 2))
-              .Concat(Observable.Timer(TimeSpan.FromSeconds(1)).Select(_ => 3))
-              .Concat(Observable.Timer(TimeSpan.FromSeconds(1)).Select(_ => 4))
-              .Concat(Observable.Timer(TimeSpan.FromSeconds(3)).Select(_ => 5));
+              .Return("Update A")
+              .Concat(Observable.Timer(TimeSpan.FromSeconds(2)).Select(_ => "Update B"))
+              .Concat(Observable.Timer(TimeSpan.FromSeconds(1)).Select(_ => "Update C"))
+              .Concat(Observable.Timer(TimeSpan.FromSeconds(1)).Select(_ => "Update D"))
+              .Concat(Observable.Timer(TimeSpan.FromSeconds(3)).Select(_ => "Update E"));
 
             observable.Throttle(TimeSpan.FromSeconds(2))
                 .RunExample("Throttle");
         }
 
-        private static void UsingDelay()
+        private static void VariableThrottling()
         {
+            Demo.DisplayHeader("Throttle operator - each element can define the throttling duration independantly by providing a function that creates an observable");
+
+var observable = Observable
+    .Return("Update A")
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(2)).Select(_ => "Update B"))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(1)).Select(_ => "Immediate Update"))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(1)).Select(_ => "Update D"))
+    .Concat(Observable.Timer(TimeSpan.FromSeconds(3)).Select(_ => "Update E"));
+
+observable
+    .Throttle(x => x == "Immediate Update"
+        ? Observable.Empty<long>()
+        : Observable.Timer(TimeSpan.FromSeconds(2)))
+    .RunExample("Variable Throttling");
+
+
+        }
+
+        private static void DelayingNotifications()
+        {
+            Demo.DisplayHeader("Delay operator - shifts the observable sequence by a timespan");
+
             var observable = Observable
                 .Timer(TimeSpan.FromSeconds(1))
                 .Concat(Observable.Timer(TimeSpan.FromSeconds(1)))
@@ -69,18 +96,31 @@ namespace UsingTimeBasedOperators
                 .Take(5)
                 .RunExample("Delay");
         }
+        private static void DelayingEachNotificationIndependently()
+        {
+            Demo.DisplayHeader("Throttle operator - each notification can be shifted independently by providing observable that signals the delay is over");
 
+            var observable = ObservableEx.FromValues(4, 1, 2, 3);
+
+            observable
+                .Timestamp()
+                .Delay(x => Observable.Timer(TimeSpan.FromSeconds(x.Value)))
+                .Timestamp()
+                .RunExample("Independent Delay");
+        }
         public static void UsingTimeout()
         {
-var observable = Observable
+            Demo.DisplayHeader("Timeout operator - enforcing a timeout if no notification was made in a period of time");
+
+            var observable = Observable
      .Timer(TimeSpan.FromSeconds(1))
      .Concat(Observable.Timer(TimeSpan.FromSeconds(1)))
      .Concat(Observable.Timer(TimeSpan.FromSeconds(4)))
      .Concat(Observable.Timer(TimeSpan.FromSeconds(4)));
 
-observable
-    .Timeout(TimeSpan.FromSeconds(3))
-    .RunExample("Timeout");
+            observable
+                .Timeout(TimeSpan.FromSeconds(3))
+                .RunExample("Timeout");
         }
 
         private static void AddingATimestampToNotifications()
@@ -124,7 +164,7 @@ observable
                 .Timer(TimeSpan.FromSeconds(1))
                 .Concat(Observable.Timer(TimeSpan.FromSeconds(2)))
                 .Concat(Observable.Timer(TimeSpan.FromSeconds(4)));
-            
+
             observable
                 .TimeInterval()
                 .RunExample("time interval");
