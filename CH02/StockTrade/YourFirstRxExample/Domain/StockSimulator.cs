a1747defcdc1efa296cdf53335e8f92f0b13ee9c
tamir.dresher@gmail.com <vhpupuyo>
2016-03-20 11:09:04 +0200

    added a simulator for the stock-ticker example

---
+++ CH02/StockTrade/YourFirstRxExample/Domain/StockSimulator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FirstRxExample
+{
+    /// <summary>
+    /// This simulator will emit a batch of StockTicks every two seconds.
+    /// each time, a single item will be selected and updated with a "drastic change" (more than 10%)
+    /// </summary>
+    public class StockSimulator
+    {
+        private readonly StockTicker _ticker;
+        private IEnumerable<StockTick> _ticks;
+        private int _itemToDrasticUpdate = 0;
+        public StockSimulator(StockTicker ticker)
+        {
+            _ticker = ticker;
+            _ticks = new[]
+            {
+                new StockTick() {QuoteSymbol = "MSFT", Price = 53.49M},
+                new StockTick() {QuoteSymbol = "INTC", Price = 32.68M},
+                new StockTick() {QuoteSymbol = "ORCL", Price = 41.48M},
+                new StockTick() {QuoteSymbol = "CSCO", Price = 28.33M},
+            };
+        }
+
+        public void Run()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    UpdatePrices();
+                    PrintPrices();
+                    Emit();
+                    Thread.Sleep(2000);
+                }
+            });
+        }
+
+        private void Emit()
+        {
+            Console.WriteLine("Emitting...");
+            foreach (var stockTick in _ticks)
+            {
+                _ticker.Notify(stockTick);
+            }
+        }
+
+        private void PrintPrices()
+        {
+            Console.WriteLine("Next series to emit:");
+            Console.WriteLine("\t");
+            foreach (var stockTick in _ticks)
+            {
+                Console.WriteLine("{{{0} - {1}}}, ", stockTick.QuoteSymbol, stockTick.Price);
+            }
+            Console.WriteLine();
+
+        }
+
+        private void UpdatePrices()
+        {
+            _ticks = _ticks.Select((tick, i) =>
+              {
+                  var changePercentage = _itemToDrasticUpdate == i ? 1.2M : 1.1M;
+                  return new StockTick() { Price = tick.Price * changePercentage, QuoteSymbol = tick.QuoteSymbol };
+              }).ToList();
+
+            _itemToDrasticUpdate++;
+            _itemToDrasticUpdate %= _ticks.Count();
+        }
+    }
+}
--- CH02/StockTrade/YourFirstRxExample/Program.cs
+++ CH02/StockTrade/YourFirstRxExample/Program.cs
@@ -11,9 +11,11 @@ namespace FirstRxExample
 {
     class Program
     {
+        private static StockTicker _stockTicker;
+
         private static void Main(string[] args)
         {
-            var stockTicker = new StockTicker();
+            _stockTicker = new StockTicker();
 
 
             /////////////////////////////////////////////////////////////
@@ -23,19 +25,56 @@ namespace FirstRxExample
             /////////////////////////////////////////////////////////////
 
             // Regular events StockMonitor
-            var stockMonitor = new StockMonitor(stockTicker);
-            
-            // Rx StockMonitor
-            //var stockMonitor = new RxStockMonitor(stockTicker);
+            //var stockMonitor = new StockMonitor(_stockTicker);
 
-            //////////////////////////////////////////////////////////////////
-            //                                                              //
-            // 2. (optional) Uncomment to test the case of Concurrent Ticks //
-            //                                                              //
-            //////////////////////////////////////////////////////////////////
+            // Rx StockMonitor - uses Rx to consume and process the stock ticks
+            var stockMonitor = new RxStockMonitor(_stockTicker);
 
-            //TestConcurrentTicks(stockTicker);
+            ShowMenu();
+          
+            GC.KeepAlive(stockMonitor);
+            Console.WriteLine("Press <enter> to continue...");
+            Console.ReadLine();
+            Console.WriteLine("Bye Bye");
+        }
 
+        private static void ShowMenu()
+        {
+
+            Console.WriteLine("Choose a simulation type (or x to exit):");
+            Console.WriteLine("1) Manual     - you enter the symbol and price");
+            Console.WriteLine("2) Automatic  - the system emits and updates a predefined collection of ticks");
+            Console.WriteLine("3) Concurrent - tests what happens when ticks are emitted concurrently");
+
+            var selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "1":
+                    ManualSimulator(_stockTicker);
+                    break;
+                case "2":
+                    AutomaticSimulator(_stockTicker);
+                    break;
+                case "3":
+                    TestConcurrentTicks(_stockTicker);
+                    break;
+                case "x":
+                    return;
+                default:
+                    Console.WriteLine("Unknow selection");
+                    return;
+            }
+
+        }
+
+        private static void AutomaticSimulator(StockTicker stockTicker)
+        {
+            var simulator = new StockSimulator(stockTicker);
+            simulator.Run();
+        }
+
+        private static void ManualSimulator(StockTicker stockTicker)
+        {
             //////////////////////////////////////////////////////
             // A small program to let you enter the Ticks info. //
             // Symbol X will exit the program                   // 
@@ -52,24 +91,21 @@ namespace FirstRxExample
                 decimal price;
                 if (decimal.TryParse(Console.ReadLine(), out price))
                 {
-                    stockTicker.Notify(new StockTick() {Price = price, QuoteSymbol = symbol});
+                    stockTicker.Notify(new StockTick() { Price = price, QuoteSymbol = symbol });
                 }
                 else
                 {
                     Console.WriteLine("price should be decimal");
                 }
             }
-
-            GC.KeepAlive(stockMonitor);
-            Console.WriteLine("Bye Bye");
         }
 
         private static void TestConcurrentTicks(StockTicker stockTicker)
         {
-            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() {Price = 100, QuoteSymbol = "MSFT"}));
-            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() {Price = 150, QuoteSymbol = "INTC"}));
-            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() {Price = 170, QuoteSymbol = "MSFT"}));
-            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() {Price = 195.5M, QuoteSymbol = "MSFT"}));
+            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() { Price = 100, QuoteSymbol = "MSFT" }));
+            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() { Price = 150, QuoteSymbol = "INTC" }));
+            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() { Price = 170, QuoteSymbol = "MSFT" }));
+            ThreadPool.QueueUserWorkItem((_) => stockTicker.Notify(new StockTick() { Price = 195.5M, QuoteSymbol = "MSFT" }));
         }
     }
 }
