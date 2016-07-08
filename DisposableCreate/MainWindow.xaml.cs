bb93fe8016547e7bb5f6d14249378ebf9c0771c7
tamir.dresher@gmail.com <vhpupuyo>
2016-07-08 19:44:08 +0300

    Disposable.Create example

---
+++ DisposableCreate/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using DisposableCreate.Annotations;
+
+namespace DisposableCreate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        private IEnumerable<string> _newsItems;
+        private bool _isBusy;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                if (value == _isBusy) return;
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            RefreshNews();
+        }
+
+        public IEnumerable<string> NewsItems
+        {
+            get { return _newsItems; }
+            set
+            {
+                if (Equals(value, _newsItems)) return;
+                _newsItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async void RefreshNews()
+        {
+            IsBusy = true;
+            NewsItems = Enumerable.Empty<string>();
+            using (Disposable.Create(() => IsBusy = false))
+            {
+                NewsItems = await DownloadNewsItems();
+            }
+        }
+
+
+        private async Task<IEnumerable<string>>  DownloadNewsItems()
+        {
+            await Task.Delay(2000);
+            return Enumerable.Range(1, 10).Select(i => "News Items " + i);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
