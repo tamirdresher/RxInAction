895f3a86a12c893795e5b33cc6c4d0eb727ea9f9
tamir.dresher@gmail.com <tamir.dresher@gmail.com>
2016-09-29 00:05:27 +0300

    Testing a Rx operator

---
+++ AppendixC-Testing/RxLibrary.Tests/FilterBurstsTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reactive.Linq;
+using Microsoft.Reactive.Testing;
+using Xunit;
+using RxLibrary;
+
+namespace RxLibrary.Tests
+{
+    public class FilterBurstsTests : ReactiveTest
+    {
+        [Fact]
+        public static void FilterBursts_SeuquenceOf10AndBurstSize5_TwoEmissions()
+        {
+            var seqeucneSize = 10;
+            var burstSize = 5;
+            var expected = new[] { 0, 5 };
+            var observable = Observable.Range(0, seqeucneSize);
+
+            observable.FilterBursts(burstSize)
+                .AssertEqual(expected.ToObservable());
+        }
+
+        [Theory]
+        [InlineData(1, 1, new[] { 0 })]
+        [InlineData(5, 1, new[] { 0 })]
+        [InlineData(1, 5, new[] { 0, 1, 2, 3, 4 })]
+        [InlineData(5, 5, new[] { 0 })]
+        [InlineData(5, 8, new[] { 0, 5 })]
+        public void FilterBursts(int burstSize, int sequenceSize, int[] expected)
+        {
+            var observable = Observable.Range(0, sequenceSize);
+            observable.FilterBursts(burstSize)
+                .AssertEqual(expected.ToObservable());
+        }
+
+        [Fact]
+        public void FilterBursts_TwoBurstAndGapInEachBurst_FirstInEachBurstEmitted()
+        {
+            
+            var scheduler = new TestScheduler();
+            var xs = scheduler.CreateHotObservable(
+                OnNext(250, 1),
+                OnNext(260, 2),
+                OnNext(270, 3),
+
+                OnNext(400, -1),
+                OnNext(401, -2),
+                OnNext(405, -3),
+
+                OnCompleted<int>(500)
+                );
+
+            var res = scheduler.Start(() => xs.FilterBursts(3));
+
+            res.Messages.AssertEqual(
+                OnNext(250, 1),
+                OnNext(400, -1),
+                OnCompleted<int>(500));
+
+            xs.Subscriptions.AssertEqual(
+                Subscribe(Subscribed, 500));
+        }
+
+        
+    }
+}
---
+++ AppendixC-Testing/RxLibrary/ObservableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace RxLibrary
+{
+    public class Teststst { }
+    public static class ObservableExtensions
+    {
+        public static IObservable<T> FilterBursts<T>(this IObservable<T> src, int burstSize)
+        {
+            return src.Window(burstSize).SelectMany(window => window.Take(1));
+        }
+
+        public static IObservable<T> FilterBursts<T>(this IObservable<T> src, TimeSpan burstDuration)
+        {
+            return src.FilterBursts(burstDuration, DefaultScheduler.Instance);
+        }
+
+        public static IObservable<T> FilterBursts<T>(this IObservable<T> src, TimeSpan burstDuration,
+            IScheduler scheduler)
+        {
+            return src.Publish(xs =>
+            {
+                var windowBoundaries = xs.Throttle(burstDuration, scheduler);
+
+                 return xs.Window(windowBoundaries).SelectMany(window => window.Take(1));
+             });
+        }
+    }
+}
